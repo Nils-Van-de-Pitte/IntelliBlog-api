2abebea46a3026f7cb8df148ce501148affbc223
Nils <nilsvandepitte9@gmail.com>
2025-06-16 17:30:27 +0200

    Add `AddBloggingContext` extension method to configure `BloggingContext` with PostgreSQL connection string

---
+++ code/IntelliBlog/IntelliBlog-backend/Extensions/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿using IntelliBlog_backend.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelliBlog_backend.Extensions;
+
+public static class BloggingContextExtension
+{
+    public static void AddBloggingContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BloggingContext>(options =>
+        {
+            var host = configuration["DB_HOST"];
+            var name = configuration["DB_NAME"];
+            var user = configuration["DB_USER"];
+            var password = configuration["DB_PASSWORD"];
+            var port = configuration["DB_PORT"];
+
+            var connectionString = $"Host={host};Port={port};Username={user};Password={password};Database={name};";
+            options.UseNpgsql(connectionString);
+        });
+    }
+}
