610a633a4ad5f37ed57c66cc08c7433e721e0a2f
Nils <nilsvandepitte9@gmail.com>
2025-06-16 14:23:04 +0200

    Add `CreateUser` feature with endpoint for user registration

---
+++ code/IntelliBlog/IntelliBlog-backend/Features/User/Create/CreateUser.cs
@@ -0,0 +1,34 @@
+﻿using FastEndpoints;
+
+namespace IntelliBlog_backend.Features.User.Create;
+
+public static class CreateUser
+{
+    public record Request(string Name, string Email, string Password);
+    public record Response(Guid Id, string Name, string Email);
+
+    public class Endpoint : Endpoint<Request, Response>
+    {
+        /// Configures the endpoint route for user registration.
+        public override void Configure()
+        {
+            Post("/api/v1/auth/register");
+            AllowAnonymous();
+            Tags("Register");
+        }
+
+        /// Handles the user registration process by processing the incoming request and returning a response.
+        /// <param name="request">
+        /// The incoming request containing user details such as name, email, and password for registration.
+        /// </param>
+        /// <param name="ct"></param>
+        /// <returns>
+        /// A result object containing the user details including ID, name, and email if registration is successful.
+        /// </returns>
+        public override async Task HandleAsync(Request request, CancellationToken ct)
+        {
+            var response = new Response(Guid.NewGuid(), request.Name, request.Email);
+            await SendOkAsync(response, ct);
+        }
+    }
+}
