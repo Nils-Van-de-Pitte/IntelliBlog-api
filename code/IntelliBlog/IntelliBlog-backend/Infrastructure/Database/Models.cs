0997c20874867c98843dbd2478408b68230bdb00
Nils <nilsvandepitte9@gmail.com>
2025-06-16 15:50:25 +0200

    Add database models for User, Blog, and Post entities

---
+++ code/IntelliBlog/IntelliBlog-backend/Infrastructure/Database/Models.cs
@@ -0,0 +1,32 @@
+﻿namespace IntelliBlog_backend.Infrastructure.Database;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string Firstname { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
+    public virtual string FullName => $"{Firstname} {LastName}";
+}
+public class Blog
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string OwnerId { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
+}
+public class Post
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Content { get; set; }
+    public required string AuthorId { get; set; }
+    public required int Likes { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
+}
