9cf8f79b43ae656317867304ea83492234ed80bf
Nils <nilsvandepitte9@gmail.com>
2025-06-16 17:30:21 +0200

    Add `BloggingContext` DbContext with DbSet properties for core entities

---
+++ code/IntelliBlog/IntelliBlog-backend/Infrastructure/Database/BloggingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntelliBlog_backend.Infrastructure.Database;
+
+public class BloggingContext: DbContext
+{
+    public BloggingContext(DbContextOptions<BloggingContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Role> Roles {get; set;}
+    public DbSet<User> Users {get; set;}
+    public DbSet<Blog> Blogs {get; set;}
+    public DbSet<Post> Posts {get; set;}
+}
