c2399502dda02cdab90818e951c628261f7b3e79
Nils <nilsvandepitte9@gmail.com>
2025-06-16 13:25:33 +0200

    Add Scalar extension for API reference integration in development environment

---
+++ code/IntelliBlog/IntelliBlog-backend/Extensions/Scalar.cs
@@ -0,0 +1,19 @@
+﻿using Scalar.AspNetCore;
+
+namespace IntelliBlog_backend.Extensions;
+
+public static class Scalar
+{
+    public static void UseScalar(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment()) return;
+        app.MapOpenApi();
+        app.MapScalarApiReference(options =>
+        {
+            options.Title = "IntelliBlog API Reference";
+            options.Theme = ScalarTheme.Kepler;
+            options.Layout = ScalarLayout.Classic;
+        });
+
+    }
+}
