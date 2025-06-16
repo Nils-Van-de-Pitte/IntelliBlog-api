924d73e1a6715f2b8831124b67202f5cfd47dc68
Nils <nilsvandepitte9@gmail.com>
2025-06-16 13:35:54 +0200

    Add `UseDependencies` extension for managing service registrations

---
+++ code/IntelliBlog/IntelliBlog-backend/Extensions/Dependencies.cs
@@ -0,0 +1,10 @@
+﻿namespace IntelliBlog_backend.Extensions;
+
+public static class Dependencies
+{
+    public static void UseDependencies(this IServiceCollection services)
+    {
+        services.AddOpenApi();
+        // Add all dependencies here
+    }
+}
