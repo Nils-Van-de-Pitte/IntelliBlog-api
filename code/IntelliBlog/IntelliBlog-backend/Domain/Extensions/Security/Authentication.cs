3773c342d1a0f894ea89ad2b2921ca93c48e1de2
Nils <nilsvandepitte9@gmail.com>
2025-06-17 16:40:18 +0200

    Add `UseSecurity` extension method to configure authentication and authorization middleware

--- code/IntelliBlog/IntelliBlog-backend/Domain/Extensions/Security/Authentication.cs
+++ code/IntelliBlog/IntelliBlog-backend/Domain/Extensions/Security/Authentication.cs
@@ -4,9 +4,23 @@ namespace IntelliBlog_backend.Domain.Extensions.Security;
 
 public static class Authentication
 {
+    /// <summary>
+    /// Adds security configurations, including authentication and authorization, to the application.
+    /// </summary>
+    /// <param name="services">The service collection to which the security configurations will be added.</param>
     public static void AddSecurity(this IServiceCollection services)
     {
         services.AddAuthenticationJwtBearer(s => s.SigningKey = "The secret used to sign the JWT");
         services.AddAuthorization();
+    }
+
+    /// <summary>
+    /// Configures the application to use authentication and authorization middleware.
+    /// </summary>
+    /// <param name="app">The web application to configure with security middleware.</param>
+    public static void UseSecurity(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
     }
 }
