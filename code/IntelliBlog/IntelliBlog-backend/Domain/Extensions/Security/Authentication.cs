f2d082c6559cda433f5381c08e4f1759148ce091
Nils <nilsvandepitte9@gmail.com>
2025-06-17 16:37:51 +0200

    Add `AddSecurity` extension method to configure JWT authentication and authorization.

---
+++ code/IntelliBlog/IntelliBlog-backend/Domain/Extensions/Security/Authentication.cs
@@ -0,0 +1,12 @@
+﻿using FastEndpoints.Security;
+
+namespace IntelliBlog_backend.Domain.Extensions.Security;
+
+public static class Authentication
+{
+    public static void AddSecurity(this IServiceCollection services)
+    {
+        services.AddAuthenticationJwtBearer(s => s.SigningKey = "The secret used to sign the JWT");
+        services.AddAuthorization();
+    }
+}
