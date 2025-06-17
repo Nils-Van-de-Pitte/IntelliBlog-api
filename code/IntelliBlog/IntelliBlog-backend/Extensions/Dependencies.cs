fa99138efa06f539a847dd6eabde2e5b59b470de
Nils <nilsvandepitte9@gmail.com>
2025-06-17 13:18:50 +0200

    Refactor `Dependencies` extension: add `RegisterServices` method and update XML comments for clarity.

--- code/IntelliBlog/IntelliBlog-backend/Extensions/Dependencies.cs
+++ code/IntelliBlog/IntelliBlog-backend/Extensions/Dependencies.cs
@@ -1,18 +1,29 @@
 ﻿using FastEndpoints;
+using IntelliBlog_backend.Domain.Interfaces;
+using IntelliBlog_backend.Infrastructure.Security;
 
 namespace IntelliBlog_backend.Extensions;
 
 public static class Dependencies
 {
     /// <summary>
-    /// Configures the service collection with required dependencies for the application.
+    /// Adds and configures dependencies for the application.
     /// </summary>
-    /// <param name="services">The service collection to configure.</param>
-    /// <param name="configuration"></param>
-    public static void UseDependencies(this IServiceCollection services, IConfiguration configuration)
+    /// <param name="services">The service collection to which the dependencies will be added.</param>
+    /// <param name="configuration">The configuration interface to retrieve configuration settings.</param>
+    public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddOpenApi();
         services.AddFastEndpoints();
         services.AddBloggingContext(configuration);
+    }
+
+    /// <summary>
+    /// Registers application-specific services into the dependency injection container.
+    /// </summary>
+    /// <param name="services">The service collection to which the services will be registered.</param>
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
     }
 }
