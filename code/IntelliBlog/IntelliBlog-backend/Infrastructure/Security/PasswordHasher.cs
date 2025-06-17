1dc9e762d1c919312ea28134f2d4806e2431761f
Nils <nilsvandepitte9@gmail.com>
2025-06-17 13:18:42 +0200

    Add `PasswordHasher` implementation skeleton for password hashing and verification

---
+++ code/IntelliBlog/IntelliBlog-backend/Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using IntelliBlog_backend.Domain.Interfaces;
+
+namespace IntelliBlog_backend.Infrastructure.Security;
+
+public sealed class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        throw new NotImplementedException();
+    }
+}
