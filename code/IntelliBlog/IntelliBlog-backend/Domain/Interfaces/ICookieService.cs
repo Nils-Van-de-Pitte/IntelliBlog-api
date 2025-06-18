4ef9c4d7c1eabbe32be4ddcac1acf744ea057018
Nils <nilsvandepitte9@gmail.com>
2025-06-18 12:48:01 +0200

    Introduce `ICookieService` interface to manage cookie operations (set, get, delete).

---
+++ code/IntelliBlog/IntelliBlog-backend/Domain/Interfaces/ICookieService.cs
@@ -0,0 +1,8 @@
+﻿namespace IntelliBlog_backend.Domain.Interfaces;
+
+public interface ICookieService
+{
+    void SetCookie(string key, string value, int? expireDays = null);
+    string? GetCookie(string key);
+    void DeleteCookie(string key);
+}
