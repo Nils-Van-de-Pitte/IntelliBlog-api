edf064496c3c7e3ea7e43e24776b27f72843e490
Nils <nilsvandepitte9@gmail.com>
2025-06-19 16:22:01 +0200

    Implement `CreatePost` endpoint to handle post creation requests with rate-limiting and summary description.

---
+++ code/IntelliBlog/IntelliBlog-backend/Features/Posts/CreatePost.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints;
+using IntelliBlog_backend.Infrastructure.Database;
+
+namespace IntelliBlog_backend.Features.Posts;
+
+public class CreatePost
+{
+    public record PostReq(string Title, string Content);
+    public record PostRes(string Message);
+
+    public sealed class Endpoint(BloggingContext context) : Endpoint<PostReq, PostRes> 
+    {
+        private readonly BloggingContext _context = context;
+
+        public override void Configure()
+        {
+            Post("/api/v1/post");
+            AllowAnonymous();
+            Description(options =>
+            {
+                options.WithSummary("Create a new post");
+            });
+            Throttle(
+                hitLimit: 15,
+                durationSeconds: 10
+            );
+        }
+    }
+}
