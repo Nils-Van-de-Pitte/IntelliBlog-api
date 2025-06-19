1125821dc9f030d7347e6cc3b0d65f1cc0e99ea7
Nils <nilsvandepitte9@gmail.com>
2025-06-19 16:11:30 +0200

    Add FluentValidation to `Register` endpoint and refactor request/response models

--- code/IntelliBlog/IntelliBlog-backend/Features/Auth/Register/Register.cs
+++ code/IntelliBlog/IntelliBlog-backend/Features/Auth/Register/Register.cs
@@ -1,4 +1,5 @@
 ﻿using FastEndpoints;
+using FluentValidation;
 using IntelliBlog_backend.Domain.Interfaces;
 using IntelliBlog_backend.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
@@ -7,10 +8,10 @@ namespace IntelliBlog_backend.Features.Auth.Register;
 
 public static class Register
 {
-    public record Request(string FirstName, string LastName, string Email, string Password);
-    public record Response(string Message);
+    public record UserReq(string FirstName, string LastName, string Email, string Password);
+    public record UserRes(string Message);
 
-    public sealed class Endpoint(BloggingContext context, IPasswordHasher passwordHasher) : Endpoint<Request, Response>
+    public sealed class Endpoint(BloggingContext context, IPasswordHasher passwordHasher) : Endpoint<UserReq, UserRes>
     {
         
         private readonly BloggingContext _context = context;
@@ -28,11 +29,11 @@ public static class Register
             );
         }
         
-        /// Handles the user registration request and processes the creation of a new user.
-        /// <param name="request">The user registration request containing Name, Email, and Password.</param>
+        /// Handles the user registration userReq and processes the creation of a new user.
+        /// <param name="userReq">The user registration userReq containing Name, Email, and Password.</param>
         /// <param name="ct">The cancellation token for the asynchronous operation.</param>
         /// <return>A task representing the asynchronous operation that returns a response with the newly created user details.</return>
-        public override async Task HandleAsync(Request request, CancellationToken ct)
+        public override async Task HandleAsync(UserReq userReq, CancellationToken ct)
         {
             var defaultRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user", ct);
 
@@ -45,11 +46,11 @@ public static class Register
             var user = new User
             {
                 Id = Guid.NewGuid(),
-                Firstname = request.FirstName,
-                LastName = request.LastName,
-                Email = request.Email,
+                Firstname = userReq.FirstName,
+                LastName = userReq.LastName,
+                Email = userReq.Email,
                 Role = defaultRole,
-                Password = _passwordHasher.HashPassword(request.Password),
+                Password = _passwordHasher.HashPassword(userReq.Password),
                 CreatedAt = DateTime.UtcNow,
                 UpdatedAt = DateTime.UtcNow
             };
@@ -57,7 +58,27 @@ public static class Register
             _context.Users.Add(user);
             await _context.SaveChangesAsync(ct);
             
-            await SendAsync(new Response("User has been successfully created!"), 201, ct);
+            await SendAsync(new UserRes("User has been successfully created!"), 201, ct);
+        }
+    }
+    
+    public class Validation : Validator<UserReq>
+    {
+        public Validation()
+        {
+            RuleFor(x => x.FirstName)
+                .MaximumLength(20)
+                .WithMessage("First name cannot exceed 20 characters.");
+            RuleFor(x => x.LastName)
+                .MaximumLength(20)
+                .WithMessage("Last name cannot exceed 20 characters.");
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .WithMessage("Email is not valid.");
+            RuleFor(x => x.Password)
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters.");
+            
         }
     }
 }
