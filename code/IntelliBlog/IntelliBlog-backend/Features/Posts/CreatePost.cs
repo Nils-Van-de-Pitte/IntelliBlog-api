df845f7671b5f971a0b1138aea9e619647c15aee
Nils <nilsvandepitte9@gmail.com>
2025-06-19 16:37:56 +0200

    Add FluentValidation to `CreatePost` endpoint and enhance error handling in `HandleAsync`.

--- code/IntelliBlog/IntelliBlog-backend/Features/Posts/CreatePost.cs
+++ code/IntelliBlog/IntelliBlog-backend/Features/Posts/CreatePost.cs
@@ -1,4 +1,5 @@
 ﻿using FastEndpoints;
+using FluentValidation;
 using IntelliBlog_backend.Infrastructure.Database;
 
 namespace IntelliBlog_backend.Features.Posts;
@@ -28,21 +29,48 @@ public class CreatePost
 
         public override async Task HandleAsync(PostReq request, CancellationToken ct)
         {
-            var post = new Post
+            try
             {
-                Id = Guid.NewGuid(),
-                Title = request.Title,
-                Content = request.Content,
-                Likes = 0,
-                BlogId = Guid.NewGuid(),
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
-            };
+                var post = new Post
+                {
+                    Id = Guid.NewGuid(),
+                    Title = request.Title,
+                    Content = request.Content,
+                    Likes = 0,
+                    BlogId = Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                };
+
+                await _context.Posts.AddAsync(post, ct);
+                await _context.SaveChangesAsync(ct);
+
+                await SendAsync(new PostRes("Post has been successfully created!"), 201, ct);
+            }
+            catch (Exception e)
+            {
+                if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                {
+                    AddError(e.Message);
+                }
+                await SendErrorsAsync(500, ct);
+            }
             
-            await _context.Posts.AddAsync(post, ct);
-            await _context.SaveChangesAsync(ct);
-            
-            await SendAsync(new PostRes("Post has been successfully created!"), 201, ct);
+        }
+    }
+
+    public sealed class Validation : Validator<PostReq>
+    {
+        public Validation()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .WithMessage("Title is required");
+            RuleFor(x => x.Content)
+                .NotEmpty()
+                .WithMessage("Content is required")
+                .MaximumLength(1000)
+                .WithMessage("Content cannot exceed 1000 characters");
         }
     }
 }
