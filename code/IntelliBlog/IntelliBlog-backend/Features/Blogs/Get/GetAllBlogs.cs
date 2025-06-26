9c8837c8c9602ace56c504aaaf275e26aa5d4129
Nils <nilsvandepitte9@gmail.com>
2025-06-26 13:19:35 +0200

    Implement `GetAllBlogs` endpoint to retrieve all blogs for the authenticated user with rate-limiting, role-based access, and proper response handling.

---
+++ code/IntelliBlog/IntelliBlog-backend/Features/Blogs/Get/GetAllBlogs.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using IntelliBlog_backend.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelliBlog_backend.Features.Blogs.Get;
+
+public class GetAllBlogs
+{
+    public record SingleBlogResDto
+    {
+        public Guid BlogId { get; init; }
+        public required string Title { get; init; }
+        public required string Description { get; init; }
+        public required int AmountOfPosts { get; init; }
+        public required int AmountOfLikes { get; init; }
+    }
+    
+    public record SingleBlogRes(string Message, List<SingleBlogResDto> Blogs);
+
+    public sealed class Endpoint(BloggingContext context) : Ep.NoReq.Res<SingleBlogRes>
+    {
+        private readonly BloggingContext _context = context;
+        
+        public override void Configure()
+        {
+            Get("api/v1/blogs");
+            Roles("user");
+            Tags("Blog");
+            Description(options =>
+                {
+                    options.WithSummary("Get all my blogs");
+                }
+            );
+            Throttle(
+                hitLimit: 15,
+                durationSeconds: 10
+            );
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var blogs = _context.Blogs.Include(x => x.Posts).Where(x => x.UserId == Guid.Parse(userId!));
+            if (!blogs.Any())
+            {
+                AddError("No Blogs found. You can create one by clicking the 'Create Blog' button on the top right corner.");
+                await SendErrorsAsync(404, ct);
+                return;
+            }
+            var blogsDto = await blogs
+                .Select(blog => new SingleBlogResDto
+                {
+                    BlogId = blog.Id,
+                    Title = blog.Title,
+                    Description = blog.Description,
+                    AmountOfPosts = blog.Posts.Count,
+                    AmountOfLikes = blog.Posts.Sum(p => p.Likes)
+                }).ToListAsync(ct);
+            
+            await SendAsync(new SingleBlogRes("Blogs retrieved successfully!", blogsDto), 200, ct);
+        }
+    }
+}
