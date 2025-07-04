16d5df54719d053160cdf82526169b45e16e1b01
Nils <nilsvandepitte9@gmail.com>
2025-07-04 14:15:32 +0200

    Add `AddCustomCors` extension method to configure CORS for the application

---
+++ code/IntelliBlog/IntelliBlog-backend/Domain/Extensions/Security/Cors.cs
@@ -0,0 +1,19 @@
+﻿namespace IntelliBlog_backend.Domain.Extensions.Security;
+
+public static class Cors
+{
+    public static void AddCustomCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(
+                name: "CorsPolicy",
+                policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000");
+                    policy.AllowCredentials();
+                }
+                );
+        });
+    }
+}
